732c4ba091d5ad926bf37fc12a248a98c87efdfa
andrewwwmatsko <matskoandrew@gmail.com>
2024-04-12 20:15:05 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", submitHandler);
+
+function submitHandler(e) {
+  e.preventDefault();
+
+  const form = e.target;
+
+  //   const emailInput = form.querySelector("[type=email]").value;
+
+  //   const passwordInput = form.querySelector("[type=password]").value;
+
+  const login = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (login === "" || !password === "") {
+    return alert("All form fields must be filled in");
+  }
+
+  console.log({
+    email: login,
+    password,
+  });
+
+  form.reset();
+}
