8a63e4f20c7b1b50149294c6ee8a124bd5fbe616
andrewwwmatsko <matskoandrew@gmail.com>
2024-04-13 16:17:03 +0300

    changes

--- js/task-4.js
+++ js/task-4.js
@@ -11,8 +11,8 @@ function submitHandler(e) {
 
   //   const passwordInput = form.querySelector("[type=password]").value;
 
-  const login = form.elements.email.value;
-  const password = form.elements.password.value;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
   if (login === "" || password === "") {
     return alert("All form fields must be filled in");
