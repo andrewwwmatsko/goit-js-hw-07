267bf8e63b1f3bfbb4c9d82eacd6fe3f8f2d475c
andrewwwmatsko <matskoandrew@gmail.com>
2024-04-13 16:19:53 +0300

    optimized code

--- js/task-4.js
+++ js/task-4.js
@@ -11,15 +11,15 @@ function submitHandler(e) {
 
   //   const passwordInput = form.querySelector("[type=password]").value;
 
-  const login = form.elements.email.value.trim();
+  const email = form.elements.email.value.trim();
   const password = form.elements.password.value.trim();
 
-  if (login === "" || password === "") {
+  if (email === "" || password === "") {
     return alert("All form fields must be filled in");
   }
 
   console.log({
-    email: login,
+    email,
     password,
   });
 
