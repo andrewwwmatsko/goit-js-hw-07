a395941e11ab5b10ea7b77a25691568430435dfd
andrewwwmatsko <matskoandrew@gmail.com>
2024-04-13 12:42:57 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+
+const colorName = document.querySelector(".color");
+
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = body.style.backgroundColor;
+});
